38fe4a3e0ab3183af12c7cc49b9869e3f5c5348a
FideranaGabriel <gabrfidrat@gmail.com>
2023-05-18 16:48:52 +0200

    fattoeInizionuovo
    fajpsdhfkasèdjfa

--- quizz.js
+++ quizz.js
@@ -1,10 +1,14 @@
 var i = 0;
+var a = 0;
+var b = 0;
+var c = 0;
 
 function checkAnswer(option) {
     var selectedOption = option.innerText;
     var question = option.previousElementSibling.innerText;
     var ris = document.getElementById("punteggio");
     
+    a++;
 
     // Esempio di logica di valutazione della risposta
     if (question === "Qual è la capitale dell'Italia?" && selectedOption === "a) Roma") {
@@ -16,5 +20,7 @@ function checkAnswer(option) {
         //console.log("Risposta errata!");
         //ris.innerText = "Score = " + i;
         //alert("Risposta errata!" + z);
+        i--;
+        ris.innerText = "Score = " + i;
     }
 }
